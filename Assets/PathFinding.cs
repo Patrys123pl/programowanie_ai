a7bd02f33653cf4d2c5732fe5ea1917b19d51ba2
Patrys123pl <Statnikpatryk@gmail.com>
2024-03-18 11:56:00 +0100

    maze

---
+++ Assets/PathFinding.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour
+{
+    public GameObject point;
+    public GameObject AI;
+
+    public void Update()
+    {
+        
+    }
+}
